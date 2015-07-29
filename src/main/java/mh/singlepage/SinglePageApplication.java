828fb5ef0f3e7f0759d07a7f6c874a90e5d810bf
Marek Hudyma <marek.hudyma@groupon.com>
2015-07-29 16:49:24 +0200

    single page

---
+++ src/main/java/mh/singlepage/SinglePageApplication.java
@@ -0,0 +1,12 @@
+package mh.singlepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinglePageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SinglePageApplication.class, args);
+    }
+}
---
+++ src/test/java/mh/singlepage/SinglePageApplicationTests.java
@@ -0,0 +1,16 @@
+package mh.singlepage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SinglePageApplication.class)
+public class SinglePageApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
