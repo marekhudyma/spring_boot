62f0e8b5ad4ee4841f1926da30ec106d70f3bcb7
Marek Hudyma <marek.hudyma@groupon.com>
2016-07-19 23:10:17 +0200

    OAUTH2 login - google + facebook

--- src/main/java/mh/springboot/controller/SecuredController.java
+++ src/main/java/mh/springboot/controller/SecuredController.java
@@ -1,28 +1,40 @@
 package mh.springboot.controller;
 
-import org.springframework.security.core.userdetails.UserDetails;
+import mh.springboot.model.security.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.security.Principal;
-
 @Controller
 public class SecuredController {
 
+    @Value("${facebook.client.clientId}")
+    private String facebookClientId;
+
+    @Value("${google.client.clientId}")
+    private String googleClientId;
+
     @RequestMapping("/secured")
-    public String secured(Model model, Principal principal){
+    public String secured(Model model){
         String username = "";
-        if (principal instanceof UserDetails) {
-            username = ((UserDetails)principal).getUsername();
-        } else if(principal instanceof OAuth2Authentication) {
-            username = principal.getName(); //number of Facebook User
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication instanceof OAuth2Authentication) {
+            String clientId = ((OAuth2Authentication)authentication).getOAuth2Request().getClientId();
+            if(facebookClientId.equals(clientId)) {
+                username = "FACEBOOK_" + authentication.getPrincipal().toString();
+            } else if(googleClientId.equals(clientId)) {
+                username = "GOOGLE_" + authentication.getPrincipal().toString();
+            }
+        } else if(authentication instanceof UsernamePasswordAuthenticationToken) {
+            username = "DATABASE_" + ((User)authentication.getPrincipal()).getId();
         }
-
         model.addAttribute("username", username);
-
         return "secured";
     }
-
 }
